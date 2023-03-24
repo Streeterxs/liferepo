740c8bd49d12a10a1878a2368691df056265dcef
Afonso Araújo Neto <afonso_mg3@hotmail.com>
2023-03-24 16:24:01 -0300

    feat(authreader): added auth reader (#8)
    * feat(authreader): added file reader, reading authlog file
    
    * feat(authreader): lock

---
+++ packages/authreader/src/fsWatcherEvents.ts
@@ -0,0 +1,5 @@
+export enum FsWatcherEvents {
+  CHANGE = 'change',
+  CLOSE = 'close',
+  ERROR = 'error'
+}
---
+++ packages/authreader/src/getAttempt.ts
@@ -0,0 +1,25 @@
+
+enum AttemptEnum {
+  LOGGED = 'logged',
+  WRONG_PASSWORD = 'wrong_password',
+  CLOSED = 'closed'
+};
+
+export const getAttempt = (lastContent: string) => {
+  console.log({lastContent});
+  const hasLogged = lastContent.includes('session opened for user') || lastContent.includes('session closed for user');
+  if (hasLogged) {
+    return AttemptEnum.LOGGED;
+  }
+
+  const hasWrongedPassword = lastContent.includes('authentication failure');
+  if (hasWrongedPassword) {
+    return AttemptEnum.WRONG_PASSWORD;
+  }
+
+  const hasClosed = lastContent.includes('incorrect password attempts') || lastContent.includes('incorrect password attempt') || lastContent.includes('auth could not identify password for')
+  if (hasClosed) {
+    return AttemptEnum.CLOSED
+  }
+  ASDASD
+};
