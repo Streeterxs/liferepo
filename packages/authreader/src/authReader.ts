77014eba7036441f5894a89ec16039d6c9e2f66e
Afonso Araújo Neto <afonso_mg3@hotmail.com>
2023-03-25 16:09:22 -0300

    Revert "fix(reset-lint): removing lint" (#12)
    * Revert "fix(reset-lint): removing lint (#11)"
    
    This reverts commit 5ac79706680ef36efd790c0e1af1874a03582426.
    
    * test
    
    * fix(eslint): fixed turbo lint

--- packages/authreader/src/authReader.ts
+++ packages/authreader/src/authReader.ts
@@ -1,29 +1,32 @@
-import { watch, readFile } from 'node:fs/promises';
-import { FsWatcherEvents } from './fsWatcherEvents';
-import { getAttempt } from './getAttempt';
+import { watch, readFile } from 'node:fs/promises'
+import { FsWatcherEvents } from './fsWatcherEvents'
+import { getAttempt } from './getAttempt'
 
-const authReader =  async() => {
-  const authPath = '/var/log/auth.log';
-  const asyncFileWatch = watch(authPath);
+const authReader = async () => {
+    const authPath = '/var/log/auth.log'
+    const asyncFileWatch = watch(authPath)
 
-  for await (const iterator of asyncFileWatch) {
-    const {eventType} = iterator;
-    if (eventType !== FsWatcherEvents.CHANGE) {
-      break;
+    for await (const iterator of asyncFileWatch) {
+        const { eventType } = iterator
+        if (eventType !== FsWatcherEvents.CHANGE) {
+            break
+        }
+
+        const fileContentBuffer = await readFile(authPath)
+        const fileContent = fileContentBuffer.toString()
+        const fileContentArray = fileContent.split('\n')
+        const filteredFileContent = fileContentArray.filter(
+            (content) => content !== ''
+        )
+
+        const lastIndexContent = filteredFileContent.length - 1
+        const lastContent = filteredFileContent[lastIndexContent]
+
+        const attempt = getAttempt(lastContent)
+
+        // eslint-disable-next-line
+        console.log({ attempt })
     }
+}
 
-    const fileContentBuffer = await readFile(authPath);
-    const fileContent = fileContentBuffer.toString();
-    const fileContentArray = fileContent.split('\n');
-    const filteredFileContent = fileContentArray.filter((content) => content !== '');
-
-    const lastIndexContent = filteredFileContent.length - 1
-    const lastContent = filteredFileContent[lastIndexContent];
-
-    const attempt = getAttempt(lastContent);
-
-    console.log({attempt});
-  }
-};
-
-(() => authReader())()
+;(() => authReader())()
--- packages/authreader/src/getAttempt.ts
+++ packages/authreader/src/getAttempt.ts
@@ -1,25 +1,29 @@
-
+// eslint-disable-next-line
 enum AttemptEnum {
-  LOGGED = 'logged',
-  WRONG_PASSWORD = 'wrong_password',
-  CLOSED = 'closed'
-};
+    LOGGED = 'logged',
+    WRONG_PASSWORD = 'wrong_password',
+    CLOSED = 'closed',
+}
 
 export const getAttempt = (lastContent: string) => {
-  console.log({lastContent});
-  const hasLogged = lastContent.includes('session opened for user') || lastContent.includes('session closed for user');
-  if (hasLogged) {
-    return AttemptEnum.LOGGED;
-  }
+    console.log({ lastContent })
+    const hasLogged =
+        lastContent.includes('session opened for user') ||
+        lastContent.includes('session closed for user')
+    if (hasLogged) {
+        return AttemptEnum.LOGGED
+    }
 
-  const hasWrongedPassword = lastContent.includes('authentication failure');
-  if (hasWrongedPassword) {
-    return AttemptEnum.WRONG_PASSWORD;
-  }
+    const hasWrongedPassword = lastContent.includes('authentication failure')
+    if (hasWrongedPassword) {
+        return AttemptEnum.WRONG_PASSWORD
+    }
 
-  const hasClosed = lastContent.includes('incorrect password attempts') || lastContent.includes('incorrect password attempt') || lastContent.includes('auth could not identify password for')
-  if (hasClosed) {
-    return AttemptEnum.CLOSED
-  }
-  ASDASD
-};
+    const hasClosed =
+        lastContent.includes('incorrect password attempts') ||
+        lastContent.includes('incorrect password attempt') ||
+        lastContent.includes('auth could not identify password for')
+    if (hasClosed) {
+        return AttemptEnum.CLOSED
+    }
+}
