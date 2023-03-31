1ccafd156ccba69cca5c79632870b116b5e781ed
Afonso Araújo Neto <afonso_mg3@hotmail.com>
2023-03-31 10:39:10 -0300

    fix(tsc): fixing typescript errors (#27)

--- packages/fcollector/src/concatJsonFile.ts
+++ packages/fcollector/src/concatJsonFile.ts
@@ -1,12 +1,12 @@
-import { writeFile, readFile } from 'node:fs/promises'
+import { writeFile, readFile } from 'node:fs/promises';
 
-export const concatJsonFile = async (path, data) => {
-  const dataBuffer = await readFile(path)
-  const dataString = dataBuffer.toString()
-  const dataJson = JSON.parse(dataString)
+export const concatJsonFile = async (path: string, data: any) => {
+  const dataBuffer = await readFile(path);
+  const dataString = dataBuffer.toString();
+  const dataJson = JSON.parse(dataString);
 
-  const newDataJson = [...dataJson, data]
-  const newDataJsonString = JSON.stringify(newDataJson)
+  const newDataJson = [...dataJson, data];
+  const newDataJsonString = JSON.stringify(newDataJson);
 
-  await writeFile(path, newDataJsonString)
-}
+  await writeFile(path, newDataJsonString);
+};
