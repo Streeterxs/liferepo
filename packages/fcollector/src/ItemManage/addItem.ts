04c9f13cd379ef3c041b16d1d517bca6ea9bfb2e
Streeterxs <afonso_mg3@hotmail.com>
2023-04-03 11:58:50 -0300

    feat(add-item): addItem fn

--- packages/fcollector/src/ItemManage/addItem.ts
+++ packages/fcollector/src/ItemManage/addItem.ts
@@ -1,16 +1,27 @@
+import { concatJsonFile } from '../concatJsonFile';
 import { getAllSlugs } from '../getAllSlugs';
+import * as yup from 'yup';
+
+const toAddKeyValueQuestions = [
+  {
+    type: 'confirm',
+    name: 'toAddKeyValue',
+    message: 'Add more key/value to item?',
+    default: true,
+  },
+];
 
 const keyValueQuestions = [
   {
     type: 'input',
     name: 'key',
-    message: 'Add the custom property key: ',
+    message: 'Add the custom property (key): ',
   },
 
   {
     type: 'input',
     name: 'value',
-    message: 'Add the custom property value: ',
+    message: 'Add the custom property (value): ',
   },
 ];
 
@@ -39,22 +50,49 @@ const questions = [
     name: 'type',
     message: 'Add a item type:',
   },
-  {
-    type: 'input',
-    name: 'values',
-    message: 'How many key/value do you need?',
-    validate(value: string) {
-      const valid = !isNaN(parseFloat(value));
-      return valid || 'Please enter a number';
-    },
-    filter: Number,
-  },
 ];
 
+export const itemTimeValSchema = yup.object().shape({
+  name: yup.string().required(),
+  slug: yup.string().required(),
+  type: yup.string().required(),
+  values: yup.array().of(
+    yup.object().shape({
+      key: yup.string().required(),
+      value: yup.mixed().required(),
+    })
+  ),
+});
+
 export const addItem = async () => {
+  const path = `${__dirname}/../data/item.json`;
   // esm package
   const inquirer = (await import('inquirer')).default;
 
-  const answers = await inquirer.prompt(questions);
-  console.log({ answers });
+  const { name, slug, type } = await inquirer.prompt(questions);
+
+  let valuesArray: { key: string; value: string }[] = [];
+  let isToAddKeyValue = true;
+  while (isToAddKeyValue) {
+    const { toAddKeyValue } = await inquirer.prompt(toAddKeyValueQuestions);
+    isToAddKeyValue = toAddKeyValue;
+
+    if (!isToAddKeyValue) {
+      break;
+    }
+
+    const { key, value } = await inquirer.prompt(keyValueQuestions);
+    valuesArray = [...valuesArray, { key, value }];
+  }
+
+  const itemObj = {
+    name,
+    slug,
+    type,
+    values: valuesArray,
+  };
+
+  await itemTimeValSchema.validate(itemObj);
+
+  await concatJsonFile(path, itemObj);
 };
