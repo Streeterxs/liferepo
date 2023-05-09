ba54c47026de15f7471bd3bf73320eef97235e48
Afonso Araújo Neto <afonso_mg3@hotmail.com>
2023-05-09 15:45:00 -0300

    feat(authreader-doc): added README to authreader package (#47)

--- packages/authreader/src/authReader.ts
+++ packages/authreader/src/authReader.ts
@@ -1,29 +1,31 @@
-import { watch, readFile } from 'node:fs/promises'
-import { FsWatcherEvents } from './fsWatcherEvents'
-import { getAttempt } from './getAttempt'
+import { watch, readFile } from 'node:fs/promises';
+import { FsWatcherEvents } from './fsWatcherEvents';
+import { getAttempt } from './getAttempt';
 
 const authReader = async () => {
-    const authPath = '/var/log/auth.log'
-    const asyncFileWatch = watch(authPath)
+  const authPath = '/var/log/auth.log';
+  const asyncFileWatch = watch(authPath);
 
-    for await (const iterator of asyncFileWatch) {
-        const { eventType } = iterator
-        if (eventType !== FsWatcherEvents.CHANGE) {
-            break
-        }
-
-        const fileContentBuffer = await readFile(authPath)
-        const fileContent = fileContentBuffer.toString()
-        const fileContentArray = fileContent.split('\n')
-        const filteredFileContent = fileContentArray.filter(
-            (content) => content !== ''
-        )
-
-        const lastIndexContent = filteredFileContent.length - 1
-        const lastContent = filteredFileContent[lastIndexContent]
-
-        const attempt = getAttempt(lastContent)
-
-        console.log({ attempt })
+  for await (const iterator of asyncFileWatch) {
+    const { eventType } = iterator;
+    if (eventType !== FsWatcherEvents.CHANGE) {
+      break;
     }
-}
+
+    const fileContentBuffer = await readFile(authPath);
+    const fileContent = fileContentBuffer.toString();
+    const fileContentArray = fileContent.split('\n');
+    const filteredFileContent = fileContentArray.filter(
+      (content) => content !== ''
+    );
+
+    const lastIndexContent = filteredFileContent.length - 1;
+    const lastContent = filteredFileContent[lastIndexContent];
+
+    const attempt = getAttempt(lastContent);
+
+    console.log({ attempt });
+  }
+};
+
+(async () => await authReader())();
